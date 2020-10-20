17c0bd9b084f28bcb27e2199cdedb7e0d51c8369
thomas.weyand <thomas@bassfreaks.de>
2020-10-20 14:57:36 +0200

    update test

--- src/com/company/Exceptionhandling.java
+++ src/com/company/Exceptionhandling.java
@@ -1,13 +1,18 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Exceptionhandling {
 
     public static void main(String[] args) throws MyException {
 
+        // Alle Exceptions in Java: https://docstore.mik.ua/orelly/java/exp/figs/je0401.gif
+
         int a = 5;
         int b = 0;
+        String vergleich = "test";
         int ergebnis = 0;
-
+/*
         // Exception abfangen, in diesem Fall eine ArithmeticException:
         try{
             ergebnis = a/b;
@@ -19,14 +24,18 @@ public class Exceptionhandling {
         } finally {
             if (ergebnis == 0) System.out.println("Ergebnis wurde nicht verändert...");  // Wird immer ausgeführt, auch wenn einee Exception aufgetreten ist.
         }
+*/
 
         // Eigene Exception:
-        if (b>0){
-            System.out.println("Hurra, wie teilen uns!");
+        Scanner scan = new Scanner(System.in);
+        System.out.print("test-Eingabe: ");
+        String eingabe = scan.next();
+        if (eingabe.equals(vergleich)){
+            System.out.println("Hurra, test-Test bestanden!");
         } else {
             throw new MyException();  // ohne try-Block muss die Methode um "throws MyException" erweitert werden, damit der darin erzeugte Fehler weiter geleitet wird
         }
-
+/*
         //Beispiel mit ArrayIndexOutOfBoundsException:
         int[] myArray = {1,2,3,4,5};
         try{
@@ -36,5 +45,8 @@ public class Exceptionhandling {
             System.out.println("Ergebnis: " + except.getCause());
             //except.printStackTrace();
         }
+
+ */
     }
 }
+
